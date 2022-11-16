3ec084ecbb4fbf805f40553e6719ecdc785a6d59
EuniceNam <caellime@gmail.com>
2022-11-16 10:45:18 +0900

    add initial project file

---
+++ StudySoilground/StudySoilground/StudySoilgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySoilgroundApp.swift
+//  StudySoilground
+//
+//  Created by Hyorim Nam on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct StudySoilgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
